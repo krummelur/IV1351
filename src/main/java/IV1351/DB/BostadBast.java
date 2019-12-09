778c8145e87d983b09fd61d6326ab0e5111c3821
Magnus <krummelur@gmail.com>
2019-12-09 21:16:29 +0100

    fix

--- src/main/java/IV1351/DB/BostadBast.java
+++ src/main/java/IV1351/DB/BostadBast.java
@@ -17,7 +17,7 @@ public class BostadBäst {
         .addColumn(new Column("namn", true, false, "varchar(128)"))
         .addColumn(new Column("multiplapplatser", false, false, "boolean"));
 
-        Table bostadsrättsgupp = new Table("Bostadsrättsgrupp")
+        Table bostadsrattsgupp = new Table("Bostadsrättsgrupp")
         .addColumn(new Column("bostadsrättsgruppsid", true, true, "integer"))
         .addColumn(new Column("namn", "varchar(128)"))
         .addColumn(new Column("bostadsrättsförening", false, false, new ForeignKey(brf, brf.getColumn("organisationsnummer")), "integer"));
@@ -37,7 +37,7 @@ public class BostadBäst {
         .addColumn(new Column("beteckning", "varchar(128)"))
         .addColumn(new Column("yta", "integer"));
 
-        Table tvättstuga = new Table("Tvättstuga")
+        Table tvattstuga = new Table("Tvättstuga")
                 .addColumn(new Column("tvättstugeid", true, true, null, "integer"))
                 .addColumn(new Column("husid", false, false,
         new ForeignKey(hus, hus.getColumn("husid")), "integer"))
@@ -47,7 +47,7 @@ public class BostadBäst {
         Table maskin = new Table("Maskin")
         .addColumn(new Column("maskintyp", "varchar(128)"))
         .addColumn(new Column("tvättstugeid", false, true,
-                new ForeignKey(tvättstuga, tvättstuga.getColumn("tvättstugeid")), "integer"))
+                new ForeignKey(tvattstuga, tvattstuga.getColumn("tvättstugeid")), "integer"))
         .addColumn(new Column("antal", false, false, null, "integer"));
 
         Table hiss = new Table("Hiss")
@@ -58,7 +58,7 @@ public class BostadBäst {
         .addColumn(new Column("husid", false, false,
                 new ForeignKey(hus, hus.getColumn("husid")), "integer"));
 
-        Table hissBestiktningsFöretag = new Table("hissbesiktningsföretag")
+        Table hissBestiktningsForetag = new Table("hissbesiktningsföretag")
         .addColumn(new Column("organisationsnummer", true, true, null, "integer"))
         .addColumn(new Column("name", true, false, null, "varchar(128)"));
 
@@ -66,7 +66,7 @@ public class BostadBäst {
         .addColumn(new Column("hissid", false, true, new ForeignKey(hiss, hiss.getColumn("hissid")), "integer"))
         .addColumn(new Column("datum", false, true, "integer"))
         .addColumn(new Column("h 9issbesiktningsföretag", false, false,
-                new ForeignKey(hissBestiktningsFöretag, hissBestiktningsFöretag.getColumn("organisationsnummer")), "integer"));
+                new ForeignKey(hissBestiktningsForetag, hissBestiktningsForetag.getColumn("organisationsnummer")), "integer"));
 
         Table bostadsrätt = new Table("Bostadsrätt")
         .addColumn(new Column("bostadsrättsid", true, true, null, "integer"))
@@ -77,19 +77,19 @@ public class BostadBäst {
         .addColumn(new Column("typ", "integer"))
         .addColumn(new Column("våning","integer"))
         .addColumn(new Column("bostadsrättsgurppsid", false, false,
-                new ForeignKey(bostadsrättsgupp, bostadsrättsgupp.getColumn("bostadsrättsgruppsid")), "integer"));
+                new ForeignKey(bostadsrattsgupp, bostadsrattsgupp.getColumn("bostadsrättsgruppsid")), "integer"));
 
-        Table årsavgift = new Table("Årsavgift")
+        Table arsavgift = new Table("Årsavgift")
                 .addColumn(new Column("verksamhetsår", false, true, null, "integer"))
                 .addColumn(new Column("bostadsrättsgruppsid", false, true, new
-                        ForeignKey(bostadsrättsgupp, bostadsrättsgupp.getColumn("bostadsrättsgruppsid")), "integer"))
+                        ForeignKey(bostadsrattsgupp, bostadsrattsgupp.getColumn("bostadsrättsgruppsid")), "integer"))
                 .addColumn(new Column("belopp", false, false, null, "integer"));
 
         Table person = new Table("Person")
                 .addColumn(new Column("namn", false, false, null, "varchar(128)"))
                 .addColumn(new Column("personnummer", true, true, null, "integer"));
 
-        Table ägandeskap = new Table("Ägandeskap")
+        Table agandeskap = new Table("Ägandeskap")
                 .addColumn(new Column("köpdatum", false, true, null, "integer"))
                 .addColumn(new Column("säljdatum", false, false, null,"integer"))
                 .addColumn(new Column("procentandel", false, false, null, "float"))
@@ -98,7 +98,7 @@ public class BostadBäst {
                 .addColumn(new Column("person", false, true,
                         new ForeignKey(person, person.getColumn("personnummer")), "integer"));
 
-        Table parkeringsköplats = new Table("Parkeringsköplats")
+        Table parkeringskoplats = new Table("Parkeringsköplats")
                 .addColumn(new Column("bostadsrättsid", false, true,
                         new ForeignKey(bostadsrätt, bostadsrätt.getColumn("bostadsrättsid")), "integer"))
                 .addColumn(new Column("tidsstämpel", false, false, null, "integer"));
@@ -110,13 +110,13 @@ public class BostadBäst {
                 .addColumn(new Column("bestrivning", false, false, null, "integer"));
 
 
-        Table köplatsrad = new Table("Köplatsrad")
+        Table koplatsrad = new Table("Köplatsrad")
                 .addColumn(new Column("typid", false, true,
                         new ForeignKey(parkeringsplatstyp, parkeringsplatstyp.getColumn("typid")), "integer"))
                 .addColumn(new Column("bostadsrättsid", false, true,
                         new ForeignKey(bostadsrätt, bostadsrätt.getColumn("bostadsrättsid")), "integer"));
 
-        Table parkeringsårsavgift = new Table("Parkeringsårsavgift")
+        Table parkeringsarsavgift = new Table("Parkeringsårsavgift")
                 .addColumn(new Column("startår", false, true, "integer"))
                 .addColumn(new Column("typid", false, true,
                         new ForeignKey(parkeringsplatstyp, parkeringsplatstyp.getColumn("typid")), "integer"))
@@ -130,8 +130,10 @@ public class BostadBäst {
                 .addColumn(new Column("bostadsrättsid", true, false,
                         new ForeignKey(bostadsrätt, bostadsrätt.getColumn("bostadsrättsid")), "integer"));
 
-        return new ArrayList<>(Arrays.asList(bostadsrättsgupp, hus, cykelRum, tvättstuga, maskin, hiss, hissBestiktningsFöretag,
-                besiktning, bostadsrätt, årsavgift, person, ägandeskap, parkeringsköplats, parkeringsplatstyp, köplatsrad,
-                parkeringsårsavgift, parkeringsplats));
+        ArrayList<Table> allTables = new ArrayList<>(Arrays.asList(brf, bostadsrattsgupp, hus, cykelRum, tvattstuga, maskin, hiss, hissBestiktningsForetag,
+                besiktning, bostadsrätt, arsavgift, person, agandeskap, parkeringskoplats, parkeringsplatstyp, koplatsrad,
+                parkeringsarsavgift, parkeringsplats));
+        allTables.forEach(t -> t.populateFromCSV());
+        return allTables;
     }
 }
